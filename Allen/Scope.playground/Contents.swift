64bc914c3b102b17dbb56d7b1a845c8d354181c7
eoc940 <hgk0000@naver.com>
2022-07-11 18:46:22 +0900

    feat: 함수 Scope 학습

---
+++ Allen/Scope.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+func greeting1() {
+    print("Hello")
+    
+    var myName = "hong"
+    
+    print(myName)
+    print(name)
+    
+    if true {
+        print(myName)
+        print(name)
+    }
+}
+
+greeting1()
+
+var name = "jobs"
+
+greeting1()
+
+
+func say1() {
+    print("hello")
+    
+    func say2() {
+        print("hey")
+    }
+    
+    say2()
+}
+
+// say2() say2만 실행 불가
+
+// command + a -> 전체 선택, ctrl + i -> 줄맞춤
+
+
